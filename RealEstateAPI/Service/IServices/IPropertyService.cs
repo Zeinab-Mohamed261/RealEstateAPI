183c682e13ed6b93cb727fa5b052910ff1ec03fa
PC <PC@DESKTOP-4QNGLLJ>
2025-06-22 13:13:30 +0300

    Add project files.

---
+++ RealEstateAPI/Service/IServices/IPropertyService.cs
@@ -0,0 +1,33 @@
+﻿using RealEstateAPI.DTOs.HomeDTOs;
+using RealEstateAPI.DTOs.PropertyDTOs;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace RealEstateAPI.Service.IServices
+{
+    public interface IPropertyService
+    {
+        Task<List<PropertyResponseDTO>> GetAllPropertiesAsync(
+            string? searchTerm,
+            string? location,
+            decimal? minPrice,
+            decimal? maxPrice,
+            int? minSize,
+            int? maxSize,
+            int? bedrooms,
+            int? bathrooms,
+            string? type,
+            string? furnishingStatus,
+            List<string>? amenities);
+        Task<HomePageResponseDTO> GetHomePageDataAsync();
+
+        Task<PropertyResponseDTO> GetPropertyByIdAsync(Guid id);
+
+        Task<PropertyResponseDTO> AddPropertyAsync(AddPropertyDTO propertyDto, string userId);
+        Task<List<string>> UploadImagesAsync(Guid propertyId, List<IFormFile> images);
+        Task<bool> UpdatePropertyAsync(UpdatePropertyDTO propertyDto, string currentUserId); 
+        Task<bool> DeletePropertyAsync(Guid id, string currentUserId); 
+    }
+}
